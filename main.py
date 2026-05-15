eb3a7b6a6f9c396d79626f132837316d3b6552cc
klfischer89 <fischer-k4@web.de>
2026-05-15 12:52:33 +0200

    Exercise 5

--- main.py
+++ main.py
@@ -29,3 +29,46 @@ def aufgabe3():
 def aufgabe4():
     value = sum(range(1,1001))
     print(value)
+
+# Aufgabe 5
+import datetime
+
+class CV():
+    firstname = ""
+    lastname = ""
+    birthday = ""
+    address = ""
+    knowledge = ""
+
+    def buildCV(self, cvData):
+        self.firstname = cvData.get("firstname")
+        self.lastname = cvData.get("lastname")
+        self.birthday = cvData.get("birthday")
+        self.address = cvData.get("address")
+        self.knowledge = cvData.get("knowledge")
+
+    def writeCV(self):
+        cvString = (
+            f"\033[32m"
+            f"My CV:\n"
+            f"Firstname: {self.firstname}\n"
+            f"Lastname: {self.lastname}\n"
+            f"Birthday: {self.birthday}\n"
+            f"Address: {self.address}\n"
+            f"Knowledge: {self.knowledge}"
+            f"\033[0m"
+        )
+        return cvString
+        
+def inputCV():
+    cvData = {}
+    cvData["firstname"] = input("Firstname: ")
+    cvData["lastname"] = input("Lastname: ")
+    cvData["birthday"] = datetime.datetime.strptime(input("Date of birth (in form YYYY-MM-DD): "), "%Y-%m-%d").date()
+    cvData["address"] = input("Address: ")
+    cvData["knowledge"] = input("Knowledge: ")
+    return cvData
+
+cv = CV()
+cv.buildCV(inputCV())
+print(cv.writeCV())
